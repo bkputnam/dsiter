df752db4d2540d78c779ce7d1cc792664171288f
Brian Putnam <brian@brian-putnam.com>
2016-12-05 21:43:42 -0700

    Convert TimesOperator to TypedBinaryOperator

--- src/main/java/blatis/operator/TimesOperator.java
+++ src/main/java/blatis/operator/TimesOperator.java
@@ -1,19 +1,16 @@
 package blatis.operator;
 
 import blatis.row.ColumnType;
-import blatis.row.IRowAccessor;
+import blatis.row.TypedRowAccessor;
 
-/**
- * Created by bkputnam on 12/3/16.
- */
-public class TimesOperator extends BinaryOperator {
+public class TimesOperator extends TypedBinaryOperator {
 
-    public TimesOperator(IRowAccessor lhs, IRowAccessor rhs) {
+    public TimesOperator(TypedRowAccessor lhs, TypedRowAccessor rhs) {
         super(lhs, rhs);
     }
 
     @Override
-    public ColumnType getType() {
+    public ColumnType getReturnType(ColumnType lhsType, ColumnType rhsType) {
         return
             eitherTypeIs(ColumnType.DOUBLE) ? ColumnType.DOUBLE :
             eitherTypeIs(ColumnType.FLOAT) ? ColumnType.FLOAT :
@@ -26,23 +23,40 @@ public class TimesOperator extends BinaryOperator {
         return lhsType.isNumeric() && rhsType.isNumeric();
     }
 
-    protected Object getValue(int lhs, int rhs) { return lhs * rhs; }
-    protected Object getValue(int lhs, long rhs) { return lhs * rhs; }
-    protected Object getValue(int lhs, float rhs) { return lhs * rhs; }
-    protected Object getValue(int lhs, double rhs) { return lhs * rhs; }
+	@Override
+	protected int handle_int_int_int(int lhs, int rhs) { return lhs * rhs; }
+	@Override
+	protected long handle_int_long_long(int lhs, long rhs) { return lhs * rhs; }
+	@Override
+	protected float handle_int_float_float(int lhs, float rhs) { return lhs * rhs; }
+	@Override
+	protected double handle_int_double_double(int lhs, double rhs) { return lhs * rhs; }
 
-    protected Object getValue(long lhs, int rhs) { return lhs * rhs; }
-    protected Object getValue(long lhs, long rhs) { return lhs * rhs; }
-    protected Object getValue(long lhs, float rhs) { return lhs * rhs; }
-    protected Object getValue(long lhs, double rhs) { return lhs * rhs; }
+	@Override
+	protected long handle_long_int_long(long lhs, int rhs) { return lhs * rhs; }
+	@Override
+	protected long handle_long_long_long(long lhs, long rhs) { return lhs * rhs; }
+	@Override
+	protected float handle_long_float_float(long lhs, float rhs) { return lhs * rhs; }
+	@Override
+	protected double handle_long_double_double(long lhs, double rhs) { return lhs * rhs; }
 
-    protected Object getValue(float lhs, int rhs) { return lhs * rhs; }
-    protected Object getValue(float lhs, long rhs) { return lhs * rhs; }
-    protected Object getValue(float lhs, float rhs) { return lhs * rhs; }
-    protected Object getValue(float lhs, double rhs) { return lhs * rhs; }
+	@Override
+	protected float handle_float_int_float(float lhs, int rhs) { return lhs * rhs; }
+	@Override
+	protected float handle_float_long_float(float lhs, long rhs) { return lhs * rhs; }
+	@Override
+	protected float handle_float_float_float(float lhs, float rhs) { return lhs * rhs; }
+	@Override
+	protected double handle_float_double_double(float lhs, double rhs) { return lhs * rhs; }
 
-    protected Object getValue(double lhs, int rhs) { return lhs * rhs; }
-    protected Object getValue(double lhs, long rhs) { return lhs * rhs; }
-    protected Object getValue(double lhs, float rhs) { return lhs * rhs; }
-    protected Object getValue(double lhs, double rhs) { return lhs * rhs; }
+	@Override
+	protected double handle_double_int_double(double lhs, int rhs) { return lhs * rhs; }
+	@Override
+	protected double handle_double_long_double(double lhs, long rhs) { return lhs * rhs; }
+	@Override
+	protected double handle_double_float_double(double lhs, float rhs) { return lhs * rhs; }
+	@Override
+	protected double handle_double_double_double(double lhs, double rhs) { return lhs * rhs; }
+
 }
