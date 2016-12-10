41d0435fd8b9a27e70c4d6c990c9c0923fc77449
Brian Putnam <brian@brian-putnam.com>
2016-12-10 14:45:47 -0700

    Add NthRootOperator

---
+++ src/main/java/blatis/operator/NthRootOperator.java
@@ -0,0 +1,78 @@
+package blatis.operator;
+
+import blatis.row.ColumnType;
+import blatis.row.IRowAccessor;
+import blatis.row.TypedRowAccessor;
+
+public class NthRootOperator extends TypedBinaryOperator {
+
+	public NthRootOperator(TypedRowAccessor lhs, TypedRowAccessor rhs) {
+		super(lhs, rhs);
+	}
+
+	@Override
+	public ColumnType getReturnType(ColumnType lhs, ColumnType rhs) {
+		return
+			super.eitherTypeIs(ColumnType.DOUBLE) ? ColumnType.DOUBLE :
+			eitherTypeIs(ColumnType.FLOAT) ? ColumnType.FLOAT :
+			eitherTypeIs(ColumnType.LONG) ? ColumnType.LONG :
+			ColumnType.INT;
+	}
+
+	@Override
+	protected boolean testTypeCompatibility(ColumnType lhsType, ColumnType rhsType) {
+		return lhsType.isNumeric() && rhsType.isNumeric();
+	}
+
+	
+	
+	@Override
+	protected int handle_int_int_int(int lhs, int rhs) { return (int)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected long handle_int_long_long(int lhs, long rhs) { return (long)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected float handle_int_float_float(int lhs, float rhs) { return (float)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected double handle_int_double_double(int lhs, double rhs) { return (double)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected long handle_long_int_long(long lhs, int rhs) { return (long)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected long handle_long_long_long(long lhs, long rhs) { return (long)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected float handle_long_float_float(long lhs, float rhs) { return (float)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected double handle_long_double_double(long lhs, double rhs) { return (double)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected float handle_float_int_float(float lhs, int rhs) { return (float)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected float handle_float_long_float(float lhs, long rhs) { return (float)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected float handle_float_float_float(float lhs, float rhs) { return (float)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected double handle_float_double_double(float lhs, double rhs) { return (double)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected double handle_double_int_double(double lhs, int rhs) { return (double)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected double handle_double_long_double(double lhs, long rhs) { return (double)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected double handle_double_float_double(double lhs, float rhs) { return (double)Math.pow(lhs, 1.0 / rhs); }
+	
+	@Override
+	protected double handle_double_double_double(double lhs, double rhs) { return (double)Math.pow(lhs, 1.0 / rhs); }
+	
+
+}
---
+++ src/test/java/blatis/operator/TestNthRootOperator.java
@@ -0,0 +1,48 @@
+package blatis.operator;
+
+import blatis.row.ConstantAccessor;
+import blatis.row.Row;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by bkputnam on 12/10/16.
+ */
+public class TestNthRootOperator {
+
+	@Test
+	public void testIntRoot() {
+		NthRootOperator no = new NthRootOperator(
+			new ConstantAccessor(8),
+			new ConstantAccessor(3)
+		);
+		assertEquals(2, no.getValueFromRow(new Row()));
+	}
+
+	@Test
+	public void testLongRoot() {
+		NthRootOperator no = new NthRootOperator(
+			new ConstantAccessor(8L),
+			new ConstantAccessor(3L)
+		);
+		assertEquals(2L, no.getValueFromRow(new Row()));
+	}
+
+	@Test
+	public void testFloatRoot() {
+		NthRootOperator no = new NthRootOperator(
+			new ConstantAccessor(8F),
+			new ConstantAccessor(3F)
+		);
+		assertEquals(2F, no.getValueFromRow(new Row()));
+	}
+
+	@Test
+	public void testDoubleRoot() {
+		NthRootOperator no = new NthRootOperator(
+			new ConstantAccessor(8D),
+			new ConstantAccessor(3D)
+		);
+		assertEquals(2D, no.getValueFromRow(new Row()));
+	}
+}
