c072129e605598d783aea2e165f739d013db6c6e
Brian Putnam <brian@brian-putnam.com>
2016-12-16 09:23:51 -0700

    Add more StdPipes

--- src/main/java/blatis/StdPipes.java
+++ src/main/java/blatis/StdPipes.java
@@ -1,16 +1,34 @@
 package blatis;
 
-import blatis.operator.parser.OperatorParser;
-import blatis.pipe.FilterPipe;
-import blatis.pipe.IPipe;
-import blatis.row.IRowAccessor;
-import blatis.row.TypedRowAccessor;
+import blatis.iterator.AbstractDatasetIterator;
+import blatis.pipe.*;
 
-/**
- * Created by bkputnam on 12/15/16.
- */
 public class StdPipes {
 	public static IPipe filter(String filter) {
 		return new FilterPipe(filter);
+	}
+
+	public static IPipe first() {
+		return new FirstPipe();
+	}
+
+	public static IPipe last() {
+		return new LastPipe();
+	}
+
+	public static IPipe rename(String from, String to) {
+		return new RenamePipe(from, to);
+	}
+
+	public static IPipe skip(int howMany) {
+		return new SkipPipe(howMany);
+	}
+
+	public static IPipe stride(int stride) {
+		return new StridePipe(stride);
+	}
+
+	public static IPipe zip(AbstractDatasetIterator... iters) {
+		return new ZipPipe(iters);
 	}
 }
