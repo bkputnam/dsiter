406c4964c59ede6903f3cb4208c963f3b7a92e7e
Brian Putnam <brian.putnam@lasp.colorado.edu>
2016-12-16 17:24:57 -0700

    Clean up RenameIterator

--- src/main/java/blatis/iterator/RenameIterator.java
+++ src/main/java/blatis/iterator/RenameIterator.java
@@ -6,13 +6,19 @@ import blatis.row.ColumnDescriptor;
 public class RenameIterator extends AbstractDatasetIterator {
 
 	private AbstractDatasetIterator src;
-	private String fromName;
-	private String toName;
+	private ColumnDescriptor[] cds;
 
 	public RenameIterator(AbstractDatasetIterator src, String from, String to) {
 		this.src = src;
-		this.fromName = from;
-		this.toName = to;
+
+		ColumnDescriptor[] srcCds = src.getColumnDescriptors();
+		cds = new ColumnDescriptor[srcCds.length];
+		for(int i=0; i<srcCds.length; i++) {
+			ColumnDescriptor srcCd = srcCds[i];
+			cds[i] = srcCd.getName() != from
+				? srcCd
+				: new ColumnDescriptor(to, srcCd.getType(), srcCd.getRowArrayIndex());
+		}
 	}
 
 	public boolean tryMoveNext() {
@@ -24,17 +30,7 @@ public class RenameIterator extends AbstractDatasetIterator {
 	}
 
 	public ColumnDescriptor[] getColumnDescriptors() {
-		ColumnDescriptor[] cds = this.src.getColumnDescriptors();
-		ColumnDescriptor[] copy = new ColumnDescriptor[cds.length];
-
-		for(int i=0; i<cds.length; i++) {
-			ColumnDescriptor cd = cds[i];
-			copy[i] = cd.getName() != fromName
-				? cd
-				: new ColumnDescriptor( toName, cd.getType(), cd.getRowArrayIndex() );
-		}
-
-		return copy;
+		return cds;
 	}
 
 }
