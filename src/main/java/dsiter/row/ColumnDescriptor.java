8d3f12a67f5be2465fe048171f6b19fd6dd692ce
Brian Putnam <brian@brian-putnam.com>
2016-12-20 20:18:10 -0700

    Javadoc for ColumnDescriptor

--- src/main/java/dsiter/row/ColumnDescriptor.java
+++ src/main/java/dsiter/row/ColumnDescriptor.java
@@ -1,5 +1,16 @@
 package dsiter.row;
 
+/**
+ * Class that describes a Column from a dataset. It contains
+ * the name of the column, the type, and the index at which
+ * it can be accessed.
+ *
+ * <p>
+ *     To actually access a column's value within a row, however,
+ *     it is usually best to use the {@link IColumnAccessor}
+ *     returned from {@link #getAccessor()}
+ * </p>
+ */
 public class ColumnDescriptor {
 
 	private String name;
