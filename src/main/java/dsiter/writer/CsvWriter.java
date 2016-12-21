4fbf4a8471438a6a8d86a01bfe731e5e8ad626c7
Brian Putnam <brian.putnam@lasp.colorado.edu>
2016-12-21 13:00:27 -0700

    Change spaces to tabs

--- src/main/java/dsiter/writer/CsvWriter.java
+++ src/main/java/dsiter/writer/CsvWriter.java
@@ -14,67 +14,67 @@ import java.io.PrintWriter;
  */
 public class CsvWriter implements IWriter {
 
-    private String columnSeparator;
-    private String lineSeparator;
-    private boolean writeHeader;
+	private String columnSeparator;
+	private String lineSeparator;
+	private boolean writeHeader;
 
-    public CsvWriter() {
-        this(",", "\n", true);
-    }
+	public CsvWriter() {
+		this(",", "\n", true);
+	}
 
-    public CsvWriter(String columnSeparator, String lineSeparator, boolean writeHeader) {
-        this.columnSeparator = columnSeparator;
-        this.lineSeparator = lineSeparator;
-        this.writeHeader = writeHeader;
-    }
+	public CsvWriter(String columnSeparator, String lineSeparator, boolean writeHeader) {
+		this.columnSeparator = columnSeparator;
+		this.lineSeparator = lineSeparator;
+		this.writeHeader = writeHeader;
+	}
 
-    @Override
-    public void writeTo(IDatasetIterator it, OutputStream outStream) {
+	@Override
+	public void writeTo(IDatasetIterator it, OutputStream outStream) {
 
-        PrintWriter out = new PrintWriter(outStream);
+		PrintWriter out = new PrintWriter(outStream);
 
-        ColumnDescriptor[] cds = it.getColumnDescriptors();
+		ColumnDescriptor[] cds = it.getColumnDescriptors();
 
-        if( writeHeader ) {
-            for(int i=0; i<cds.length; i++) {
-                out.print(cds[i].getName());
-                if(i<cds.length-1) {
-                    out.print(columnSeparator);
-                }
-            }
-            out.print(lineSeparator);
-        }
+		if( writeHeader ) {
+			for(int i=0; i<cds.length; i++) {
+				out.print(cds[i].getName());
+				if(i<cds.length-1) {
+					out.print(columnSeparator);
+				}
+			}
+			out.print(lineSeparator);
+		}
 
-        IColumnAccessor[] cas = new IColumnAccessor[cds.length];
-        for(int i=0; i<cds.length; i++) {
-            cas[i] = cds[i].getAccessor();
-        }
+		IColumnAccessor[] cas = new IColumnAccessor[cds.length];
+		for(int i=0; i<cds.length; i++) {
+			cas[i] = cds[i].getAccessor();
+		}
 
-        while(it.tryMoveNext()) {
-            Row row = it.getCurrentRow();
+		while(it.tryMoveNext()) {
+			Row row = it.getCurrentRow();
 
-            for(int i=0; i<cas.length; i++) {
-                IColumnAccessor ca = cas[i];
-                ColumnType type = ca.getType();
+			for(int i=0; i<cas.length; i++) {
+				IColumnAccessor ca = cas[i];
+				ColumnType type = ca.getType();
 
-                if(type == ColumnType.STRING) {
-                    out.print("\"");
-                    out.print(ca.getValueFromRow(row));
-                    out.print("\"");
-                }
-                else {
-                    out.print(ca.getValueFromRow(row));
-                }
+				if(type == ColumnType.STRING) {
+					out.print("\"");
+					out.print(ca.getValueFromRow(row));
+					out.print("\"");
+				}
+				else {
+					out.print(ca.getValueFromRow(row));
+				}
 
-                if(i<cas.length-1) {
-                    out.print(columnSeparator);
-                }
-            }
+				if(i<cas.length-1) {
+					out.print(columnSeparator);
+				}
+			}
 
-            out.print(lineSeparator);
-            out.flush();
-        }
+			out.print(lineSeparator);
+			out.flush();
+		}
 
-        out.close();
-    }
+		out.close();
+	}
 }
