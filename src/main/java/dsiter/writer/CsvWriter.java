b3827be57a7b70ac277b8bf38d97f9bf563e54a5
Brian Putnam <brian.putnam@lasp.colorado.edu>
2016-12-21 16:24:39 -0700

    Javadoc for writers

--- src/main/java/dsiter/writer/CsvWriter.java
+++ src/main/java/dsiter/writer/CsvWriter.java
@@ -7,7 +7,8 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 
 /**
- * Created by bkputnam on 12/3/16.
+ * An implementation of {@link IWriter} that writes an
+ * iterator as CSV.
  */
 public class CsvWriter implements IWriter {
 
@@ -15,16 +16,37 @@ public class CsvWriter implements IWriter {
 	private String lineSeparator;
 	private boolean writeHeader;
 
+	/**
+	 * Construct a CsvWriter with default
+	 * parameters; equivalent to
+	 * {@code new CsvWriter(",", "\n", true)}
+	 */
 	public CsvWriter() {
 		this(",", "\n", true);
 	}
 
+	/**
+	 * Construct a CsvWriter with the given {@code columnSeparator} and
+	 * {@code lineSeparator}; if {@code writeHeader} is {@code true}, then
+	 * output a header as the first line.
+	 *
+	 * @param columnSeparator The column separtor to use
+	 * @param lineSeparator   The line separator to use
+	 * @param writeHeader     If {@code true}, write a header as the first line, else
+	 *                        only write data lines.
+	 */
 	public CsvWriter(String columnSeparator, String lineSeparator, boolean writeHeader) {
 		this.columnSeparator = columnSeparator;
 		this.lineSeparator = lineSeparator;
 		this.writeHeader = writeHeader;
 	}
 
+	/**
+	 * Write the passed iterator to {@code outStream} as CSV
+	 *
+	 * @param it        The iterator to be written
+	 * @param outStream The OutputStream to be written to
+	 */
 	@Override
 	public void writeTo(IDatasetIterator it, OutputStream outStream) {
 
--- src/main/java/dsiter/writer/IWriter.java
+++ src/main/java/dsiter/writer/IWriter.java
@@ -5,8 +5,17 @@ import dsiter.iterator.IDatasetIterator;
 import java.io.OutputStream;
 
 /**
- * Created by bkputnam on 12/3/16.
+ * Interface for classes that consume {@link IDatasetIterator} instances
+ * and write them to an {@link OutputStream} in some format or another.
  */
 public interface IWriter {
+
+	/**
+	 * Write the passed iterator to the passed OutputStream, in an
+	 * implementation-specific format.
+	 *
+	 * @param it        The iterator to be written
+	 * @param outStream The OutputStream to be written to
+	 */
     public void writeTo(IDatasetIterator it, OutputStream outStream);
 }
