f5a0b12f528b0d4c83c523d6546e68d6d44daaaa
Brian Putnam <brian@brian-putnam.com>
2016-12-28 12:15:19 -0500

    Update tests to use try syntax

--- src/test/java/dsiter/TestStdPipes.java
+++ src/test/java/dsiter/TestStdPipes.java
@@ -9,44 +9,56 @@ public class TestStdPipes {
 
 	@Test
 	public void testFilterPipe() throws Exception {
-		IDatasetIterator iter = new RangeIterator(20)
-			.pipe(filter("value%3=0"));
-		IterUtils.assertValues(iter, "value", new Integer[] { 0, 3, 6, 9, 12, 15, 18 });
+		try (IDatasetIterator iter = new RangeIterator(20)
+			.pipe(filter("value%3=0"))
+		) {
+			IterUtils.assertValues(iter, "value", new Integer[]{0, 3, 6, 9, 12, 15, 18});
+		}
 	}
 
 	@Test
 	public void testFirstPipe() throws Exception {
-		IDatasetIterator iter = new RangeIterator(20)
-			.pipe(first());
-		IterUtils.assertValues(iter, "value", new Integer[] { 0 });
+		try (IDatasetIterator iter = new RangeIterator(20)
+			.pipe(first())
+		) {
+			IterUtils.assertValues(iter, "value", new Integer[]{0});
+		}
 	}
 
 	@Test
 	public void testLastPipe() throws Exception {
-		IDatasetIterator iter = new RangeIterator(20)
-			.pipe(last());
-		IterUtils.assertValues(iter, "value", new Integer[] { 19 });
+		try (IDatasetIterator iter = new RangeIterator(20)
+			.pipe(last())
+		) {
+			IterUtils.assertValues(iter, "value", new Integer[]{19});
+		}
 	}
 
 	@Test
 	public void testRenamePipe() throws Exception {
-		IDatasetIterator iter = new RangeIterator(5)
-			.pipe(rename("value", "i"));
-		IterUtils.assertValues(iter, "i", new Integer[] { 0, 1, 2, 3, 4 });
+		try (IDatasetIterator iter = new RangeIterator(5)
+			.pipe(rename("value", "i"))
+		) {
+			IterUtils.assertValues(iter, "i", new Integer[]{0, 1, 2, 3, 4});
+		}
 	}
 
 	@Test
 	public void testSkipPipe() throws Exception {
-		IDatasetIterator iter = new RangeIterator(10)
-			.pipe(skip(5));
-		IterUtils.assertValues(iter, "value", new Integer[] { 5, 6, 7, 8, 9 });
+		try (IDatasetIterator iter = new RangeIterator(10)
+			.pipe(skip(5))
+		) {
+			IterUtils.assertValues(iter, "value", new Integer[]{5, 6, 7, 8, 9});
+		}
 	}
 
 	@Test
 	public void testStridePipe() throws Exception {
-		IDatasetIterator iter = new RangeIterator(10)
-			.pipe(stride(3));
-		IterUtils.assertValues(iter, "value", new Integer[] { 0, 3, 6, 9 });
+		try (IDatasetIterator iter = new RangeIterator(10)
+			.pipe(stride(3))
+		) {
+			IterUtils.assertValues(iter, "value", new Integer[]{0, 3, 6, 9});
+		}
 	}
 
 	@Test
@@ -56,17 +68,19 @@ public class TestStdPipes {
 		IDatasetIterator iter1 = new ArrayIterator(intVals);
 		IDatasetIterator iter2 = new ArrayIterator(strVals).pipe(rename("value", "s"));
 
-		IDatasetIterator zi = iter1.pipe(zip(iter2));
+		try (IDatasetIterator zi = iter1.pipe(zip(iter2))) {
 
-		IteratorExpectations e = new IteratorExpectations();
-		e.expectInts("value", intVals);
-		e.expectStrings("s", strVals);
-		e.checkIterator(zi);
+			IteratorExpectations e = new IteratorExpectations();
+			e.expectInts("value", intVals);
+			e.expectStrings("s", strVals);
+			e.checkIterator(zi);
+		}
 	}
 
 	@Test
 	public void testTake() throws Exception {
-		IDatasetIterator it = new RangeIterator(10).pipe(take(5));
-		IterUtils.assertValues(it, "value", new Integer[] { 0, 1, 2, 3, 4 });
+		try (IDatasetIterator it = new RangeIterator(10).pipe(take(5))) {
+			IterUtils.assertValues(it, "value", new Integer[]{0, 1, 2, 3, 4});
+		}
 	}
 }
--- src/test/java/dsiter/pipe/TestPipes.java
+++ src/test/java/dsiter/pipe/TestPipes.java
@@ -16,23 +16,24 @@ public class TestPipes {
 
     @Test
     public void testStride5() throws Exception {
-
-        IDatasetIterator it = new RangeIterator(25)
-                .pipe(new StridePipe(5));
-        IterUtils.assertValues(it, "value", new Integer[] {0, 5, 10, 15, 20});
+        try (IDatasetIterator it = new RangeIterator(25)
+                .pipe(new StridePipe(5))
+		) {
+			IterUtils.assertValues(it, "value", new Integer[]{0, 5, 10, 15, 20});
+		}
     }
 
     @Test
     public void testLastPipe() throws Exception {
-
-        IDatasetIterator it = new RangeIterator(25)
-                .pipe(new LastPipe());
-        IterUtils.assertValues(it, "value", new Integer[] {24});
+        try (IDatasetIterator it = new RangeIterator(25)
+                .pipe(new LastPipe())
+		) {
+			IterUtils.assertValues(it, "value", new Integer[]{24});
+		}
     }
 
     @Test
     public void testFilterPipe() throws Exception {
-
 		IRowAccessor.BOOLEAN isEvenPredicate = new EqualsOperator(
                 new ModuloOperator(
 					IColumnAccessor.getInstance(ColumnType.INT, 0),
@@ -41,43 +42,46 @@ public class TestPipes {
                 ConstantAccessor.getInstance(0)
         ).asBoolAccessor();
 
-        IDatasetIterator it = new RangeIterator(10)
-                .pipe(new FilterPipe(isEvenPredicate));
-        IterUtils.assertValues(it, "value", new Integer[] {0, 2, 4, 6, 8});
+        try (IDatasetIterator it = new RangeIterator(10)
+                .pipe(new FilterPipe(isEvenPredicate))
+		) {
+			IterUtils.assertValues(it, "value", new Integer[]{0, 2, 4, 6, 8});
+		}
     }
 
     @Test
     public void testFirstAndSkipPipes() throws Exception {
-
-        IDatasetIterator it = new RangeIterator(10)
+        try (IDatasetIterator it = new RangeIterator(10)
                 .pipe(new SkipPipe(5))
-                .pipe(new FirstPipe());
-        IterUtils.assertValues(it, "value", new Integer[] {5});
+                .pipe(new FirstPipe())
+		) {
+			IterUtils.assertValues(it, "value", new Integer[]{5});
+		}
     }
 
     @Test
     public void testRenamePipe() throws Exception {
-
-        IDatasetIterator it = new RangeIterator(5)
-                .pipe(new RenamePipe("value", "a"));
-
-        IterUtils.assertValues(it, "a", new Integer[] {0, 1, 2, 3, 4});
+        try (IDatasetIterator it = new RangeIterator(5)
+                .pipe(new RenamePipe("value", "a"))
+		) {
+			IterUtils.assertValues(it, "a", new Integer[]{0, 1, 2, 3, 4});
+		}
     }
 
     @Test
     public void testZipPipe() throws Exception {
-
-        IDatasetIterator it = new RangeIterator(5)
+        try (IDatasetIterator it = new RangeIterator(5)
             .pipe(
                 new ZipPipe(
                     new ArrayIterator(1, 3, 5, 7, 9)
                         .pipe(new RenamePipe("value", "a"))
                 )
-            );
-
-		IteratorExpectations e = new IteratorExpectations();
-		e.expectInts("value", 0, 1, 2, 3, 4);
-		e.expectInts("a", 1, 3, 5, 7, 9);
-		e.checkIterator(it);
+            )
+		) {
+			IteratorExpectations e = new IteratorExpectations();
+			e.expectInts("value", 0, 1, 2, 3, 4);
+			e.expectInts("a", 1, 3, 5, 7, 9);
+			e.checkIterator(it);
+		}
     }
 }
