33238dc1f7bde14c7dbe05f6e114f63aedb5bf3d
Brian Putnam <brian@brian-putnam.com>
2017-01-14 11:59:33 -0700

    Add testClosesSrcIter for TakeWhileIterator

--- src/test/java/dsiter/iterator/TestTakeWhileIterator.java
+++ src/test/java/dsiter/iterator/TestTakeWhileIterator.java
@@ -4,6 +4,7 @@ import dsiter.IterUtils;
 import dsiter.operator.parser.OperatorParser;
 import dsiter.row.IRowAccessor;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestTakeWhileIterator {
 
@@ -24,5 +25,27 @@ public class TestTakeWhileIterator {
 		IterUtils.assertValues(twi, "value", new Integer[] {
 			1,2,3
 		});
+	}
+
+	@Test
+	public void testClosesSrcIter() throws Exception {
+
+		IteratorCounter counter = new IteratorCounter();
+		IDatasetIterator src = new RangeIterator(10).pipe(counter.getPipe());
+
+		IRowAccessor.BOOLEAN predicate = OperatorParser.parseOperator(
+			src.getColumnDescriptors(),
+			"value!=4"
+		).asBoolAccessor();
+
+		try (
+			TakeWhileIterator it = new TakeWhileIterator(src, predicate)
+		) {
+			IterUtils.assertValues(it, "value", new Integer[] {
+				0,1,2,3
+			});
+		}
+
+		assertEquals(1, counter.getCloseCount());
 	}
 }
