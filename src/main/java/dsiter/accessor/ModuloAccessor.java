8e9b016d130de33250a512d1f1c2dd2a43c42df6
Brian Putnam <brian@brian-putnam.com>
2017-01-16 21:11:02 -0700

    Some more accessors

---
+++ src/main/java/dsiter/accessor/ModuloAccessor.java
@@ -0,0 +1,77 @@
+/* GENERATED CODE */package dsiter.accessor;
+/* GENERATED CODE */
+/* GENERATED CODE */import dsiter.row.IRowAccessor;
+/* GENERATED CODE */import dsiter.row.Row;
+/* GENERATED CODE */
+/* GENERATED CODE */public interface ModuloAccessor extends IRowAccessor {
+/* GENERATED CODE */
+/* GENERATED CODE */	
+/* GENERATED CODE */	public interface INT extends ModuloAccessor, IRowAccessor.INT { }
+/* GENERATED CODE */	public interface LONG extends ModuloAccessor, IRowAccessor.LONG { }
+/* GENERATED CODE */
+/* GENERATED CODE */	
+/* GENERATED CODE */	public class INT_INT implements INT {
+/* GENERATED CODE */
+/* GENERATED CODE */		private IRowAccessor.INT lhs;
+/* GENERATED CODE */		private IRowAccessor.INT rhs;
+/* GENERATED CODE */
+/* GENERATED CODE */		public INT_INT(IRowAccessor.INT lhs, IRowAccessor.INT rhs) {
+/* GENERATED CODE */			this.lhs = lhs;
+/* GENERATED CODE */			this.rhs = rhs;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public int getIntFromRow(Row row) {
+/* GENERATED CODE */			return lhs.getIntFromRow(row) % rhs.getIntFromRow(row);
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public class INT_LONG implements LONG {
+/* GENERATED CODE */
+/* GENERATED CODE */		private IRowAccessor.INT lhs;
+/* GENERATED CODE */		private IRowAccessor.LONG rhs;
+/* GENERATED CODE */
+/* GENERATED CODE */		public INT_LONG(IRowAccessor.INT lhs, IRowAccessor.LONG rhs) {
+/* GENERATED CODE */			this.lhs = lhs;
+/* GENERATED CODE */			this.rhs = rhs;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public long getLongFromRow(Row row) {
+/* GENERATED CODE */			return lhs.getIntFromRow(row) % rhs.getLongFromRow(row);
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public class LONG_INT implements LONG {
+/* GENERATED CODE */
+/* GENERATED CODE */		private IRowAccessor.LONG lhs;
+/* GENERATED CODE */		private IRowAccessor.INT rhs;
+/* GENERATED CODE */
+/* GENERATED CODE */		public LONG_INT(IRowAccessor.LONG lhs, IRowAccessor.INT rhs) {
+/* GENERATED CODE */			this.lhs = lhs;
+/* GENERATED CODE */			this.rhs = rhs;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public long getLongFromRow(Row row) {
+/* GENERATED CODE */			return lhs.getLongFromRow(row) % rhs.getIntFromRow(row);
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public class LONG_LONG implements LONG {
+/* GENERATED CODE */
+/* GENERATED CODE */		private IRowAccessor.LONG lhs;
+/* GENERATED CODE */		private IRowAccessor.LONG rhs;
+/* GENERATED CODE */
+/* GENERATED CODE */		public LONG_LONG(IRowAccessor.LONG lhs, IRowAccessor.LONG rhs) {
+/* GENERATED CODE */			this.lhs = lhs;
+/* GENERATED CODE */			this.rhs = rhs;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public long getLongFromRow(Row row) {
+/* GENERATED CODE */			return lhs.getLongFromRow(row) % rhs.getLongFromRow(row);
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */}
