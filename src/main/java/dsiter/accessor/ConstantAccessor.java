2b26849d9ecf12a629ea5e340e0f8196cff85293
Brian Putnam <brian@brian-putnam.com>
2017-01-16 21:57:08 -0700

    Last few Accessors

---
+++ src/main/java/dsiter/accessor/ConstantAccessor.java
@@ -0,0 +1,100 @@
+/* GENERATED CODE */package dsiter.accessor;
+/* GENERATED CODE */
+/* GENERATED CODE */import dsiter.row.IRowAccessor;
+/* GENERATED CODE */import dsiter.row.Row;
+/* GENERATED CODE */
+/* GENERATED CODE */public interface ConstantAccessor extends IRowAccessor {
+/* GENERATED CODE */
+/* GENERATED CODE */	
+/* GENERATED CODE */	public static class INT implements ConstantAccessor, IRowAccessor.INT {
+/* GENERATED CODE */		private int value;
+/* GENERATED CODE */
+/* GENERATED CODE */		public INT (int value) {
+/* GENERATED CODE */			this.value = value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public int getIntFromRow(Row row) {
+/* GENERATED CODE */			return value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public static class LONG implements ConstantAccessor, IRowAccessor.LONG {
+/* GENERATED CODE */		private long value;
+/* GENERATED CODE */
+/* GENERATED CODE */		public LONG (long value) {
+/* GENERATED CODE */			this.value = value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public long getLongFromRow(Row row) {
+/* GENERATED CODE */			return value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public static class FLOAT implements ConstantAccessor, IRowAccessor.FLOAT {
+/* GENERATED CODE */		private float value;
+/* GENERATED CODE */
+/* GENERATED CODE */		public FLOAT (float value) {
+/* GENERATED CODE */			this.value = value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public float getFloatFromRow(Row row) {
+/* GENERATED CODE */			return value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public static class DOUBLE implements ConstantAccessor, IRowAccessor.DOUBLE {
+/* GENERATED CODE */		private double value;
+/* GENERATED CODE */
+/* GENERATED CODE */		public DOUBLE (double value) {
+/* GENERATED CODE */			this.value = value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public double getDoubleFromRow(Row row) {
+/* GENERATED CODE */			return value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public static class STRING implements ConstantAccessor, IRowAccessor.STRING {
+/* GENERATED CODE */		private String value;
+/* GENERATED CODE */
+/* GENERATED CODE */		public STRING (String value) {
+/* GENERATED CODE */			this.value = value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public String getStringFromRow(Row row) {
+/* GENERATED CODE */			return value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public static class BOOLEAN implements ConstantAccessor, IRowAccessor.BOOLEAN {
+/* GENERATED CODE */		private boolean value;
+/* GENERATED CODE */
+/* GENERATED CODE */		public BOOLEAN (boolean value) {
+/* GENERATED CODE */			this.value = value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public boolean getBoolFromRow(Row row) {
+/* GENERATED CODE */			return value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */	public static class JSDATE implements ConstantAccessor, IRowAccessor.JSDATE {
+/* GENERATED CODE */		private long value;
+/* GENERATED CODE */
+/* GENERATED CODE */		public JSDATE (long value) {
+/* GENERATED CODE */			this.value = value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */
+/* GENERATED CODE */		@Override
+/* GENERATED CODE */		public long getJsDateFromRow(Row row) {
+/* GENERATED CODE */			return value;
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */	
+/* GENERATED CODE */}
