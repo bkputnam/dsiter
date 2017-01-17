62c4b4db87a95966eac2aa828550ca5b9d6df440
Brian Putnam <brian.putnam@lasp.colorado.edu>
2017-01-17 16:57:28 -0700

    Add comparison operators, tests

--- src/main/java/dsiter/parser/ast/LessThanEqualsOperator.java
+++ src/main/java/dsiter/parser/ast/LessThanEqualsOperator.java
@@ -3,14 +3,14 @@
 /* GENERATED CODE */import dsiter.row.ColumnDescriptor;
 /* GENERATED CODE */import dsiter.row.ColumnType;
 /* GENERATED CODE */import dsiter.row.IRowAccessor;
-/* GENERATED CODE */import dsiter.accessor.GreaterThanAccessor;
+/* GENERATED CODE */import dsiter.accessor.LessThanEqualsAccessor;
 /* GENERATED CODE */
-/* GENERATED CODE */public class GreaterThanOperator implements AstNode {
+/* GENERATED CODE */public class LessThanEqualsOperator implements AstNode {
 /* GENERATED CODE */
 /* GENERATED CODE */	private AstNode lhsNode;
 /* GENERATED CODE */	private AstNode rhsNode;
 /* GENERATED CODE */
-/* GENERATED CODE */	public GreaterThanOperator(AstNode lhsNode, AstNode rhsNode) {
+/* GENERATED CODE */	public LessThanEqualsOperator(AstNode lhsNode, AstNode rhsNode) {
 /* GENERATED CODE */		this.lhsNode = lhsNode;
 /* GENERATED CODE */		this.rhsNode = rhsNode;
 /* GENERATED CODE */	}
@@ -25,134 +25,134 @@
 /* GENERATED CODE */
 /* GENERATED CODE */		
 /* GENERATED CODE */		if (lhsType == ColumnType.INT && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_INT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.INT_INT(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_LONG(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.INT_LONG(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.FLOAT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_FLOAT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.INT_FLOAT(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.FLOAT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.DOUBLE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_DOUBLE(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.INT_DOUBLE(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_INT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.LONG_INT(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_LONG(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.LONG_LONG(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.FLOAT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_FLOAT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.LONG_FLOAT(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.FLOAT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.DOUBLE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_DOUBLE(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.LONG_DOUBLE(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.FLOAT && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.FLOAT_INT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.FLOAT_INT(
 /* GENERATED CODE */				(IRowAccessor.FLOAT)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.FLOAT && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.FLOAT_LONG(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.FLOAT_LONG(
 /* GENERATED CODE */				(IRowAccessor.FLOAT)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.FLOAT && rhsType == ColumnType.FLOAT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.FLOAT_FLOAT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.FLOAT_FLOAT(
 /* GENERATED CODE */				(IRowAccessor.FLOAT)lhs,
 /* GENERATED CODE */				(IRowAccessor.FLOAT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.FLOAT && rhsType == ColumnType.DOUBLE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.FLOAT_DOUBLE(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.FLOAT_DOUBLE(
 /* GENERATED CODE */				(IRowAccessor.FLOAT)lhs,
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.DOUBLE && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.DOUBLE_INT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.DOUBLE_INT(
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.DOUBLE && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.DOUBLE_LONG(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.DOUBLE_LONG(
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.DOUBLE && rhsType == ColumnType.FLOAT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.DOUBLE_FLOAT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.DOUBLE_FLOAT(
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)lhs,
 /* GENERATED CODE */				(IRowAccessor.FLOAT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.DOUBLE && rhsType == ColumnType.DOUBLE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.DOUBLE_DOUBLE(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.DOUBLE_DOUBLE(
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)lhs,
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.JSDATE && rhsType == ColumnType.JSDATE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.JSDATE_JSDATE(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.JSDATE_JSDATE(
 /* GENERATED CODE */				(IRowAccessor.JSDATE)lhs,
 /* GENERATED CODE */				(IRowAccessor.JSDATE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.JSDATE && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.JSDATE_INT(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.JSDATE_INT(
 /* GENERATED CODE */				(IRowAccessor.JSDATE)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.JSDATE && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.JSDATE_LONG(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.JSDATE_LONG(
 /* GENERATED CODE */				(IRowAccessor.JSDATE)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.JSDATE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_JSDATE(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.INT_JSDATE(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.JSDATE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.JSDATE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_JSDATE(
+/* GENERATED CODE */			return new LessThanEqualsAccessor.LONG_JSDATE(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.JSDATE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else {
 /* GENERATED CODE */			throw new LinkException(
-/* GENERATED CODE */				"Unsupported operand types for GreaterThanOperator: (" +
+/* GENERATED CODE */				"Unsupported operand types for LessThanEqualsOperator: (" +
 /* GENERATED CODE */				lhsType + ", " + rhsType + ")"
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
--- src/main/java/dsiter/parser/ast/LessThanOperator.java
+++ src/main/java/dsiter/parser/ast/LessThanOperator.java
@@ -3,14 +3,14 @@
 /* GENERATED CODE */import dsiter.row.ColumnDescriptor;
 /* GENERATED CODE */import dsiter.row.ColumnType;
 /* GENERATED CODE */import dsiter.row.IRowAccessor;
-/* GENERATED CODE */import dsiter.accessor.GreaterThanAccessor;
+/* GENERATED CODE */import dsiter.accessor.LessThanAccessor;
 /* GENERATED CODE */
-/* GENERATED CODE */public class GreaterThanOperator implements AstNode {
+/* GENERATED CODE */public class LessThanOperator implements AstNode {
 /* GENERATED CODE */
 /* GENERATED CODE */	private AstNode lhsNode;
 /* GENERATED CODE */	private AstNode rhsNode;
 /* GENERATED CODE */
-/* GENERATED CODE */	public GreaterThanOperator(AstNode lhsNode, AstNode rhsNode) {
+/* GENERATED CODE */	public LessThanOperator(AstNode lhsNode, AstNode rhsNode) {
 /* GENERATED CODE */		this.lhsNode = lhsNode;
 /* GENERATED CODE */		this.rhsNode = rhsNode;
 /* GENERATED CODE */	}
@@ -25,134 +25,134 @@
 /* GENERATED CODE */
 /* GENERATED CODE */		
 /* GENERATED CODE */		if (lhsType == ColumnType.INT && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_INT(
+/* GENERATED CODE */			return new LessThanAccessor.INT_INT(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_LONG(
+/* GENERATED CODE */			return new LessThanAccessor.INT_LONG(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.FLOAT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_FLOAT(
+/* GENERATED CODE */			return new LessThanAccessor.INT_FLOAT(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.FLOAT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.DOUBLE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_DOUBLE(
+/* GENERATED CODE */			return new LessThanAccessor.INT_DOUBLE(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_INT(
+/* GENERATED CODE */			return new LessThanAccessor.LONG_INT(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_LONG(
+/* GENERATED CODE */			return new LessThanAccessor.LONG_LONG(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.FLOAT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_FLOAT(
+/* GENERATED CODE */			return new LessThanAccessor.LONG_FLOAT(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.FLOAT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.DOUBLE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_DOUBLE(
+/* GENERATED CODE */			return new LessThanAccessor.LONG_DOUBLE(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.FLOAT && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.FLOAT_INT(
+/* GENERATED CODE */			return new LessThanAccessor.FLOAT_INT(
 /* GENERATED CODE */				(IRowAccessor.FLOAT)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.FLOAT && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.FLOAT_LONG(
+/* GENERATED CODE */			return new LessThanAccessor.FLOAT_LONG(
 /* GENERATED CODE */				(IRowAccessor.FLOAT)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.FLOAT && rhsType == ColumnType.FLOAT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.FLOAT_FLOAT(
+/* GENERATED CODE */			return new LessThanAccessor.FLOAT_FLOAT(
 /* GENERATED CODE */				(IRowAccessor.FLOAT)lhs,
 /* GENERATED CODE */				(IRowAccessor.FLOAT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.FLOAT && rhsType == ColumnType.DOUBLE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.FLOAT_DOUBLE(
+/* GENERATED CODE */			return new LessThanAccessor.FLOAT_DOUBLE(
 /* GENERATED CODE */				(IRowAccessor.FLOAT)lhs,
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.DOUBLE && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.DOUBLE_INT(
+/* GENERATED CODE */			return new LessThanAccessor.DOUBLE_INT(
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.DOUBLE && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.DOUBLE_LONG(
+/* GENERATED CODE */			return new LessThanAccessor.DOUBLE_LONG(
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.DOUBLE && rhsType == ColumnType.FLOAT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.DOUBLE_FLOAT(
+/* GENERATED CODE */			return new LessThanAccessor.DOUBLE_FLOAT(
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)lhs,
 /* GENERATED CODE */				(IRowAccessor.FLOAT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.DOUBLE && rhsType == ColumnType.DOUBLE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.DOUBLE_DOUBLE(
+/* GENERATED CODE */			return new LessThanAccessor.DOUBLE_DOUBLE(
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)lhs,
 /* GENERATED CODE */				(IRowAccessor.DOUBLE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.JSDATE && rhsType == ColumnType.JSDATE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.JSDATE_JSDATE(
+/* GENERATED CODE */			return new LessThanAccessor.JSDATE_JSDATE(
 /* GENERATED CODE */				(IRowAccessor.JSDATE)lhs,
 /* GENERATED CODE */				(IRowAccessor.JSDATE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.JSDATE && rhsType == ColumnType.INT) {
-/* GENERATED CODE */			return new GreaterThanAccessor.JSDATE_INT(
+/* GENERATED CODE */			return new LessThanAccessor.JSDATE_INT(
 /* GENERATED CODE */				(IRowAccessor.JSDATE)lhs,
 /* GENERATED CODE */				(IRowAccessor.INT)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.JSDATE && rhsType == ColumnType.LONG) {
-/* GENERATED CODE */			return new GreaterThanAccessor.JSDATE_LONG(
+/* GENERATED CODE */			return new LessThanAccessor.JSDATE_LONG(
 /* GENERATED CODE */				(IRowAccessor.JSDATE)lhs,
 /* GENERATED CODE */				(IRowAccessor.LONG)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.JSDATE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.INT_JSDATE(
+/* GENERATED CODE */			return new LessThanAccessor.INT_JSDATE(
 /* GENERATED CODE */				(IRowAccessor.INT)lhs,
 /* GENERATED CODE */				(IRowAccessor.JSDATE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.JSDATE) {
-/* GENERATED CODE */			return new GreaterThanAccessor.LONG_JSDATE(
+/* GENERATED CODE */			return new LessThanAccessor.LONG_JSDATE(
 /* GENERATED CODE */				(IRowAccessor.LONG)lhs,
 /* GENERATED CODE */				(IRowAccessor.JSDATE)rhs
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
 /* GENERATED CODE */		else {
 /* GENERATED CODE */			throw new LinkException(
-/* GENERATED CODE */				"Unsupported operand types for GreaterThanOperator: (" +
+/* GENERATED CODE */				"Unsupported operand types for LessThanOperator: (" +
 /* GENERATED CODE */				lhsType + ", " + rhsType + ")"
 /* GENERATED CODE */			);
 /* GENERATED CODE */		}
