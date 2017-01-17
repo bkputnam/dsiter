6c29a16ce81014931c14c614a69aef6509e365e6
Brian Putnam <brian.putnam@lasp.colorado.edu>
2017-01-17 15:29:57 -0700

    Add RegexAccessor tests

---
+++ src/test/java/dsiter/parser/TestRegexParsing.java
@@ -0,0 +1,52 @@
+package dsiter.parser;
+
+import dsiter.IterUtils;
+import dsiter.iterator.ArrayIterator;
+import dsiter.iterator.IDatasetIterator;
+import org.junit.Test;
+
+import static dsiter.StdPipes.filter;
+import static dsiter.StdPipes.rename;
+import static dsiter.StdPipes.zip;
+
+public class TestRegexParsing {
+
+	@Test
+	public void testFilterAndParser() throws Exception {
+
+		String[] samples = new String[] {
+			"W",
+			"WW",
+			"WaW",
+			"WaaaaaaW",
+			"aaaaaWaaaaaaWaaaaa",
+			"aaaaaWaaaaaaaaaaa"
+		};
+
+		String[] expected = new String[] {
+			"WW",
+			"WaW",
+			"WaaaaaaW",
+			"aaaaaWaaaaaaWaaaaa"
+		};
+
+		IDatasetIterator it = ArrayIterator.fromStrings(samples).pipe(filter("value~\"W.*W\""));
+
+		IterUtils.assertValues(it, "value", expected);
+	}
+
+	@Test(expected=RegexParseException.class)
+	public void testParserException1() throws Exception {
+		IDatasetIterator it = ArrayIterator.fromStrings().pipe(filter("value~3"));
+	}
+
+	@Test(expected=RegexParseException.class)
+	public void testParserException2() throws Exception {
+		IDatasetIterator it = ArrayIterator.fromStrings()
+			.pipe(zip(
+				ArrayIterator.fromStrings(new String[0]).pipe(rename("value", "foo"))
+			));
+		IDatasetIterator it2 = it.pipe(filter("value~foo"));
+	}
+
+}
