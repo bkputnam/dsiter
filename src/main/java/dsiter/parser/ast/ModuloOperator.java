961227e432bfb4c89072faba4082d6b54ffbe713
Brian Putnam <brian.putnam@lasp.colorado.edu>
2017-01-17 17:10:32 -0700

    Add ModuloOperator, tests

---
+++ src/main/java/dsiter/parser/ast/ModuloOperator.java
@@ -0,0 +1,63 @@
+/* GENERATED CODE */package dsiter.parser.ast;
+/* GENERATED CODE */
+/* GENERATED CODE */import dsiter.accessor.ModuloAccessor;
+/* GENERATED CODE */import dsiter.row.ColumnDescriptor;
+/* GENERATED CODE */import dsiter.row.ColumnType;
+/* GENERATED CODE */import dsiter.row.IRowAccessor;
+/* GENERATED CODE */
+/* GENERATED CODE */public class ModuloOperator implements AstNode {
+/* GENERATED CODE */
+/* GENERATED CODE */	private AstNode lhsNode;
+/* GENERATED CODE */	private AstNode rhsNode;
+/* GENERATED CODE */
+/* GENERATED CODE */	public ModuloOperator(AstNode lhsNode, AstNode rhsNode) {
+/* GENERATED CODE */		this.lhsNode = lhsNode;
+/* GENERATED CODE */		this.rhsNode = rhsNode;
+/* GENERATED CODE */	}
+/* GENERATED CODE */
+/* GENERATED CODE */	
+/* GENERATED CODE */
+/* GENERATED CODE */	@Override
+/* GENERATED CODE */	public IRowAccessor link(ColumnDescriptor[] cds) throws LinkException {
+/* GENERATED CODE */		IRowAccessor lhs = lhsNode.link(cds);
+/* GENERATED CODE */		IRowAccessor rhs = rhsNode.link(cds);
+/* GENERATED CODE */
+/* GENERATED CODE */		ColumnType lhsType = lhs.getType();
+/* GENERATED CODE */		ColumnType rhsType = rhs.getType();
+/* GENERATED CODE */
+/* GENERATED CODE */		
+/* GENERATED CODE */		if (lhsType == ColumnType.INT && rhsType == ColumnType.INT) {
+/* GENERATED CODE */			return new ModuloAccessor.INT_INT(
+/* GENERATED CODE */				(IRowAccessor.INT)lhs,
+/* GENERATED CODE */				(IRowAccessor.INT)rhs
+/* GENERATED CODE */			);
+/* GENERATED CODE */		}
+/* GENERATED CODE */		
+/* GENERATED CODE */		else if (lhsType == ColumnType.INT && rhsType == ColumnType.LONG) {
+/* GENERATED CODE */			return new ModuloAccessor.INT_LONG(
+/* GENERATED CODE */				(IRowAccessor.INT)lhs,
+/* GENERATED CODE */				(IRowAccessor.LONG)rhs
+/* GENERATED CODE */			);
+/* GENERATED CODE */		}
+/* GENERATED CODE */		
+/* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.INT) {
+/* GENERATED CODE */			return new ModuloAccessor.LONG_INT(
+/* GENERATED CODE */				(IRowAccessor.LONG)lhs,
+/* GENERATED CODE */				(IRowAccessor.INT)rhs
+/* GENERATED CODE */			);
+/* GENERATED CODE */		}
+/* GENERATED CODE */		
+/* GENERATED CODE */		else if (lhsType == ColumnType.LONG && rhsType == ColumnType.LONG) {
+/* GENERATED CODE */			return new ModuloAccessor.LONG_LONG(
+/* GENERATED CODE */				(IRowAccessor.LONG)lhs,
+/* GENERATED CODE */				(IRowAccessor.LONG)rhs
+/* GENERATED CODE */			);
+/* GENERATED CODE */		}
+/* GENERATED CODE */		
+/* GENERATED CODE */		else {
+/* GENERATED CODE */			throw new LinkException("Unsupported operand types for ModuloOperator: (" +
+/* GENERATED CODE */				lhsType + ", " + rhsType + ")"
+/* GENERATED CODE */			);
+/* GENERATED CODE */		}
+/* GENERATED CODE */	}
+/* GENERATED CODE */}
