836706c2bf4328db7db41b598abedf06bb49f7bb
Brian Putnam <bkputnam@gmail.com>
2018-08-01 21:31:40 -0600

    Make CacheIterator internal to CacheDataset

--- src/main/java/dsiter/dataset/CacheDataset.java
+++ src/main/java/dsiter/dataset/CacheDataset.java
@@ -1,7 +1,8 @@
 package dsiter.dataset;
 
-import dsiter.iterator.CacheIterator;
 import dsiter.iterator.IDatasetIterator;
+import dsiter.pipe.IPipe;
+import dsiter.pipe.SkipPipe;
 import dsiter.row.ColumnDescriptor;
 import dsiter.row.Row;
 import dsiter.row.RowCopier;
@@ -95,5 +96,62 @@ public class CacheDataset implements IDataset {
 	public IDatasetIterator getIterator() throws Exception {
 		maybeRefreshCache();
 		return new CacheIterator(cache, cds);
+	}
+
+	private class CacheIterator implements IDatasetIterator {
+
+		private Row[] cache;
+		private int index = -1;
+
+		private ColumnDescriptor[] cds;
+
+		private Row outputRow;
+		private RowCopier rowCopier;
+
+
+		public CacheIterator(Row[] cache, ColumnDescriptor[] cds) {
+			this.cache = cache;
+			this.cds = cds;
+
+			rowCopier = new RowCopier(cds);
+			outputRow = new Row(rowCopier.getDestShape());
+		}
+
+		@Override
+		public boolean tryMoveNext() throws Exception {
+			index++;
+			return index < cache.length;
+		}
+
+		@Override
+		public Row getCurrentRow() throws Exception {
+			rowCopier.copyTo(cache[index], outputRow);
+			return outputRow;
+		}
+
+		@Override
+		public long tryGetLength() {
+			return cache.length;
+		}
+
+		@Override
+		public ColumnDescriptor[] getColumnDescriptors() {
+			return cds;
+		}
+
+		@Override
+		public boolean tryAbsorb(IPipe pipe) {
+			if (pipe instanceof SkipPipe) {
+				long howMany = ((SkipPipe)pipe).getHowMany();
+				index += howMany;
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		@Override
+		public void close() throws Exception { }
 	}
 }
